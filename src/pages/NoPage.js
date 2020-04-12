7488595f8bcf028ce500500330d0a2ea96d146dc
zhb12345 <https://github.com/zhb12345/vue.git>
2020-04-12 21:45:51 +0800

    Update NoPage.js

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom'
 export default class NoPage extends Component{
     render() {
         return (
-            <div>
+            <React.Fragment>
                 <h2>404</h2>
                 <h2>404</h2>
                 <h2>404</h2>
                 <h2>404</h2>
                 <h2>404</h2>
                 <Link to={'/home'}>回到首页</Link>
-            </div>
+            </React.Fragment>
         )
     }
 }
